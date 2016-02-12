cc31ef0fb2300084acdddd5c9f104dc63be0e164
sachin <sachin@ranosys.com>
2016-02-12 15:49:07 +0530

    add minified file

--- Yii2-grid-view/Yii2-gridview-pjax.js
+++ Yii2-grid-view/Yii2-gridview-pjax.js
@@ -1,1 +1,55 @@
-var input,submit_form=!1,filter_selector=".grid-view input",actionColunm=null;$(document).off("keydown.yiiGridView change.yiiGridView",filter_selector).on("keyup",filter_selector,function(){var i=$(this);actionColunm=i,input=$(this).attr("name"),submit_form=!1,submit_form===!1&&(submit_form=!0,setTimeout(function(){$(".grid-view").yiiGridView("applyFilter")},1e3))}).on("pjax:success",function(){submit_form=!1,console.log("pjax success : "+submit_form);var i=$("[name='"+input+"']"),o=i.val();i.focus().val(o)}).on("pjax:error",function(){submit_form=!1;var i=$("[name='"+input+"']"),o=i.val();i.focus().val(o)}),$(document).on("beforeFilter",".grid-view",function(i){return submit_form}),$(document).on("afterFilter",".grid-view",function(i){submit_form=!1}),$(function(){$(document).on("pjax:complete",function(i){$(document).find(actionColunm).focus(),i.preventDefault(),i.stopPropagation()})});
+
+/*
+ * Yii2 grid view filter on keypress 
+ */
+
+
+
+var input;
+var submit_form = false;
+var filter_selector = '.grid-view input';
+var actionColunm = null;
+$(document)
+        .off('keydown.yiiGridView change.yiiGridView', filter_selector)
+        .on('keyup', filter_selector, function () {
+            var thisElem = $(this);
+            actionColunm = thisElem;
+            input = $(this).attr('name');
+            submit_form = false;
+
+            if (submit_form === false) {
+                submit_form = true;
+                setTimeout(function () {
+                    $(".grid-view").yiiGridView("applyFilter");
+                }, 1000);
+            }
+        })
+        .on('pjax:success', function () {
+            submit_form = false;
+            console.log('pjax success : ' + submit_form);
+            var i = $("[name='" + input + "']");
+            var val = i.val();
+            i.focus().val(val);
+        })
+        .on('pjax:error', function () {
+            submit_form = false;
+            var i = $("[name='" + input + "']");
+            var val = i.val();
+            i.focus().val(val);
+        });
+$(document).on('beforeFilter', ".grid-view", function (event) {
+    return submit_form;
+});
+
+$(document).on('afterFilter', ".grid-view", function (event) {
+    submit_form = false;
+});
+
+$(function () {
+    $(document).on('pjax:complete', function (event) {
+        $(document).find(actionColunm).focus();
+        event.preventDefault();
+        event.stopPropagation();
+        //return false;
+    });
+});
