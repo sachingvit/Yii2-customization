fd5195ecd1225d273c74562dd7f94732c20b715c
sachin <sachin@ranosys.com>
2016-02-23 12:01:24 +0530

    Pjax based grid vide search script customization for responsive and pjax abort issue handle

---
+++ Yii2-grid-view/v1.1/Yii2-gridview-pjax.js
@@ -0,0 +1,100 @@
+/*
+ * Description: Yii2 grid view filter on keypress 
+ * Author: Sachin Jain
+ * Added on : 23 Feb,2016 
+ * v1.1 
+ */
+
+var input;
+var submit_form = false;
+var filter_selector = '.grid-view input';
+var actionColunm = null;
+var abortCount = 0;
+
+
+
+
+var timer = 0;
+var timerProcess = false;
+$(document)
+        .off('keydown.yiiGridView change.yiiGridView', filter_selector)
+        .on('keyup', filter_selector, function () {
+            timer = 0;
+            timerProcess = true;
+
+            var thisElem = $(this);
+            actionColunm = thisElem;
+            input = $(this).attr('name');
+            submit_form = false;
+
+
+
+
+            if (timerProcess) {
+                var intervalThread = setInterval(function () {
+                    if (timer > 3 && timerProcess) {
+
+                        if (submit_form === false && timerProcess) {
+                            submit_form = true;
+                            setTimeout(function () {
+                                $(".grid-view").yiiGridView("applyFilter");
+                            }, 500);
+                        }
+                        timerProcess = false;
+                        timer = 0;
+                        clearInterval(intervalThread);
+                    } else {
+//                        console.log(timer > 3);
+                        if (timer > 3) {
+                            clearInterval(intervalThread);
+                        }
+                        timer++;
+
+                    }
+                }, 500);
+            }
+
+
+        })
+        .on('pjax:success', function (event) {
+            abortCount = 0;
+            submit_form = false;
+            var i = $("[name='" + input + "']");
+            var val = i.val();
+//            i.val(val).focus();
+            event.preventDefault();
+            event.stopPropagation();
+        })
+        .on('pjax:error', function (event) {
+            submit_form = false;
+            var i = $("[name='" + input + "']");
+            var val = i.val();
+            abortCount++;
+//            i.val(val).focus();
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        })
+        .on('pjax:complete', function (event) {
+            if (abortCount > 10) {
+                location.reload();
+            }
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+            submit_form = false;
+            var i = $("[name='" + input + "']");
+//            var val = i.val();
+//            i.val(val).focus();
+        });
+$(document).on('beforeFilter', ".grid-view", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return submit_form;
+});
+
+$(document).on('afterFilter', ".grid-view", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    submit_form = false;
+});
